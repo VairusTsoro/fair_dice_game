60a10bb1105690d55ffae13789ee9afda541ffbd
VairusTsoro <70801958+VairusTsoro@users.noreply.github.com>
2025-07-27 11:33:32 +0400

    Minor Changes

--- DiceParser.js
+++ DiceParser.js
@@ -1,7 +1,7 @@
 import Dice from './Dice.js';
 export default class DiceParser {
     static parse(args) {
-        if (args.length < 2) throw new Error('Need >= 2 dices.');
+        if (args.length < 3) throw new Error('Need >= 3 dices.');
         return args.map(str => {
             const nums = str.split(',').map(n => Number(n));
             if (nums.length !== 6 || nums.some(isNaN)) throw new Error(`Incorrect die: ${str}`);
